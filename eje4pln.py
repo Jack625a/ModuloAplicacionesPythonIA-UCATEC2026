d2b642cb7d6356164574dc485b673ea22fa2e8e4
Jack625a <ktfut15@gmail.com>
2026-03-11 21:28:30 -0400

    bASICO NLTK

---
+++ eje4pln.py
@@ -0,0 +1,16 @@
+#dESCARGAR EL DATA DE NLTK
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+
+texto="El Presidente Paz propone bloque de seis naciones para potenciar el desarrollo sudamericano el 20 de abril "
+#Tokenizacion por palabras
+tokenizar=word_tokenize(texto)
+#tokenizacion por oraciones
+oraciones=sent_tokenize(texto)
+#Eliminaion de palabras vacias
+palabrasVacias=set(stopwords.words('spanish'))
+restultadoPalabrasVacias=[palabra for palabra in tokenizar if palabra not in palabrasVacias]
+print(tokenizar)
+print(oraciones)
+print(restultadoPalabrasVacias)
